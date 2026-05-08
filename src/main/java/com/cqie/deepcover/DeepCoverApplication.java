d9b755b4feb7f6901d2b45cf03e396bce3272060
Jack kupol <15877767+jack-kupol@user.noreply.gitee.com>
2026-05-08 09:28:16 +0800

    feat(core): 初始化Spring Boot应用框架及配置

---
+++ src/main/java/com/cqie/deepcover/DeepCoverApplication.java
@@ -0,0 +1,13 @@
+package com.cqie.deepcover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepCoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepCoverApplication.class, args);
+    }
+
+}
